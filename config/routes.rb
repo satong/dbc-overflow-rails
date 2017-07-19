f3306884e26a1e94b722e23857e2a8c58ae44c99
satong <stephanietong@gmail.com>
2017-07-19 09:03:05 -0700

    Change links to path

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,22 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/login', to: 'sessions#new', as: 'new_login'
+  post '/login', to: 'sessions#create', as: 'login'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 
-  resources :questions do
+  resources :questions, shallow: true do
     resources :answers
   end
+
+  resources :questions, shallow: true do
+    resources :comments
+  end
+
+  resources :answers, shallow: true do
+    resources :comments
+  end
+
 
 
 
